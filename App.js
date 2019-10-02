4af08866ce17da560cd1c453a3b8353cacaed0b6
Bcardoso2 <38514089+Bcardoso2@users.noreply.github.com>
2019-10-02 09:28:57 -0300

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Hello World!</Text>
+      <Text>This is the Hello World from React Native</Text>
       </View>
     );
   }
